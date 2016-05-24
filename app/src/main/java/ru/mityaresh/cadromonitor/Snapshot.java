88ed7e82debc15334db14afb9142b44be1c7d2c3
ru-pacman <mitya@MacBook-Pro-Mitya.local>
2016-05-24 19:14:32 +0300

    v.0.2.2

--- app/src/main/java/ru/mityaresh/cadromonitor/Snapshot.java
+++ app/src/main/java/ru/mityaresh/cadromonitor/Snapshot.java
@@ -1,6 +1,18 @@
 package ru.mityaresh.cadromonitor;
 
-
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,38 +23,52 @@ import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLDecoder;
 import java.util.Timer;
 import java.util.TimerTask;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.Service;
-import android.content.Context;
-import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.os.IBinder;
-import android.util.Log;
-import org.json.JSONException;
-import org.json.JSONObject;
 
+public class Snapshot extends AppCompatActivity {
 
-public class MyService extends Service {
-    NotificationManager nm;
+    private ImageView mImageView;
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snapshot);
 
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        new Timer().schedule(new TimerTask() {
+        mImageView = (ImageView) findViewById(R.id.snapshotfrc);
+
+        //GetPic getPicTask = new GetPic();
+        //getPicTask.execute();
+
+        TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
-                sendNotif();
+                GetPic getPicTask = new GetPic();
+                getPicTask.execute();
             }
-        }, 0, 15000);
-        return START_STICKY;
+        };
+
+        Timer timer = new Timer();
+
+        timer.schedule(timerTask, 0, 1000);
+    }
+
+    class GetPic extends AsyncTask<Void, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(Void... voids) {
+            return showPic();
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            try {
+                mImageView.setImageBitmap(result);
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private boolean isNetworkConnected() {
@@ -67,11 +93,6 @@ public class MyService extends Service {
 
     }
 
-    /**
-     * Convert an input stream to a buffered reader.
-     * @param in An InputStream.
-     * @return A BufferedReader or <code>null</code> if something went wrong.
-     */
     private static BufferedReader streamToReader(InputStream in) {
         BufferedReader br;
         try {
@@ -100,7 +121,7 @@ public class MyService extends Service {
         try {
             url = new URL(urlString);
         } catch (MalformedURLException e) {
-            Log.e("MyService", e.getMessage());
+            Log.e("Snapshot", e.getMessage());
         }
 
         try {
@@ -118,54 +139,47 @@ public class MyService extends Service {
         return readData(reader);
     }
 
-    void sendNotif() {
+    private static Bitmap getCamImage(String url) {
+        try {
+            String decodedUrl = URLDecoder.decode(url, "UTF-8");
+            decodedUrl = decodedUrl.substring(0, 1 + decodedUrl.indexOf('=')) + "snapshot";
+            return BitmapFactory.decodeStream((InputStream) new URL(decodedUrl).getContent());
+        } catch (MalformedURLException e) {
+            Log.e("Snapshot", e.getMessage());
+        } catch (UnsupportedEncodingException e) {
+            Log.e("Snapshot", e.getMessage());
+        } catch (IOException e) {
+            Log.e("Snapshot", e.getMessage());
+        }
+        return null;
+    }
+    Bitmap showPic() {
         if (isNetworkConnected() && isInternetAvailable()) {
             String result = "";
             try {
                 result = httpGet("http://cadrspace.ru/status/json");
             } catch (IOException e) {
-                Log.e("MyService", e.getMessage());
-            }
-
-            Boolean isOpen = false;
-            try {
-                JSONObject jObject = new JSONObject(result);
-                isOpen = jObject.getJSONObject("state").getBoolean("open");
-            } catch (JSONException e) {
-                Log.e("MyService", e.getMessage());
+                Log.e("Snapshot", e.getMessage());
             }
 
             Context context = getApplicationContext();
-            Intent notificationIntent = null;
-            if (isOpen) {
-                 notificationIntent = new Intent(context, Snapshot.class);
-            } else {
-                 notificationIntent = new Intent();
+
+            Resources res = context.getResources();
+
+            Boolean isOpen = false;
+            Bitmap snap = BitmapFactory.decodeResource(res, R.drawable.nosnap);
+            try {
+                JSONObject jObject = new JSONObject(result);
+                isOpen = jObject.getJSONObject("state").getBoolean("open");
+                if (isOpen) {
+                    String camUrl = jObject.getJSONArray("cam").get(0).toString();
+                    snap = getCamImage(camUrl);
+                }
+                return snap;
+            } catch (JSONException e) {
+                Log.e("Snapshot", e.getMessage());
             }
-
-            PendingIntent contentIntent;
-            contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
-            Notification.Builder builder = new Notification.Builder(context);
-
-            builder.setContentIntent(contentIntent)
-                    .setSmallIcon(R.drawable.nf_cadr)
-                    .setTicker(getString(R.string.notyTitle) + " " + getString(isOpen ? R.string.open : R.string.close))
-                    .setWhen(System.currentTimeMillis())
-                    .setAutoCancel(false)
-                    .setOngoing(true)
-                    .setContentTitle(getString(R.string.notyTitle))
-                    .setContentText(getString(isOpen ? R.string.open : R.string.close));
-
-            Notification notification = builder.build();
-
-
-            NotificationManager notificationManager = (NotificationManager) context
-                    .getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.notify(101, notification);
         }
-    }
-
-    public IBinder onBind(Intent arg0) {
         return null;
     }
 }
